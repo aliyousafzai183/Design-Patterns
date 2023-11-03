a7e62533d03851f00ffcec52b7a7585bcad73915
Ali Yousafzai <warisallah923@gmail.com>
2023-11-03 10:16:52 +0500

    Implemented strategy pattern

---
+++ Design Pattern/src/StrategyPattern/src/strategypattern/StrategyPattern.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package strategypattern;
+
+/**
+ *
+ * @author FA20-BSE-165
+ */
+public class StrategyPattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Context context = new Context(new OperationAdd());		
+      System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+      context = new Context(new OperationSubstract());		
+      System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+      context = new Context(new OperationMultiply());		
+      System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+   }
+    
+}
