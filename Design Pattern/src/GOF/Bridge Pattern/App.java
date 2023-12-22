0a894fc91a19f009393801b37334cc83ff39f841
aliyousafzai183 <warisallah923@gmail.com>
2023-12-22 09:43:47 +0500

    Implemented Bridge Pattern From Refactoring Guru

---
+++ Design Pattern/src/GOF/Bridge Pattern/App.java
@@ -0,0 +1,25 @@
+import devices.Device;
+import devices.Tv;
+import remote.AdvancedRemote;
+import devices.Radio;
+import remote.BasicRemote;
+
+public class App {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
